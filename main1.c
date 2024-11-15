79a624b2e432561d3804d203f2c40268525b0f91
jamil1001 <c243029.ugrad.iiuc.ac.bd>
2024-11-15 17:10:34 +0600

    placing the character in the window

--- main1.c
+++ main1.c
@@ -1,3 +1,19 @@
+
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Example originally created with raylib 4.5, last time updated with raylib 4.5
+*
+*   Example contributed by <user_name> (@<user_github>) and reviewed by Ramon Santamaria (@raysan5)
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2023 <user_name> (@<user_github>)
+*
+********************************************************************************************/
+
 #include "raylib.h"
 
 //------------------------------------------------------------------------------------
@@ -7,12 +23,15 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
+    const int screenWidth = 1000;
+    const int screenHeight = 600;
+    
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    const char *myfile = "scarfy.png";
+    Texture2D scarfy = LoadTexture(myfile);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -20,7 +39,7 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        // TODO: Update variables / Implement example logic at this point
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -29,14 +48,20 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            // TODO: Draw everything that requires to be drawn at this point:
 
+            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);  // Example
+
+        DrawTexture(scarfy , 15 ,45 ,WHITE);
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+
+    // TODO: Unload all loaded resources at this point
+
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
