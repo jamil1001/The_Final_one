598f641e3f50d28f62f431e4a80ec128dfa39906
jamil1001 <c243029.ugrad.iiuc.ac.bd>
2024-11-16 19:29:05 +0600

    now trying 2nd part

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   RayLib 2D Challenge - An animated character
+*   raylib [core] example - Basic window
 *
 *   @author Hans de Ruiter
 *
@@ -28,41 +28,101 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 
+const int screenWidth = 1600;
+const int screenHeight = 900;
+
+const int scarfySpeed = 10;
+const int gravity = 1;
+const int groundYPos = (3 * screenHeight) / 4;
+
+const int jumpUpFrame = 3;
+const int jumpDownFrame = 4;
+const int footstepFrame1 = 1;
+const int footstepFrame2 = 4;
+
+bool isFootstepFrame(int frameIndex) {
+	
+	if(frameIndex == footstepFrame1 || frameIndex == footstepFrame2) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool isScarfyOnGround(Texture2D *scarfy, Vector2 *scarfyPosition) {
+	if(scarfyPosition->y + scarfy->height >= groundYPos) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/** Returns true if the texture is valid (e.g., was loaded).
+ */
 bool isTextureValid(const Texture2D *texture) {
 	return texture->id > 0;
+}
+
+/** Displays a single line error message, and exits when the user pushes ESCape, or the window's close
+ * button.
+ */
+void showErrorAndExit(const char *errMsg) {
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText(errMsg, 20, 20, 20, BLACK);
+		EndDrawing();
+	}
+	
+	exit(EXIT_FAILURE);
+}
+
+void showCantLoadFileErrorAndExit(const char *filename) {
+	showErrorAndExit(TextFormat("ERROR: Couldn't load %s.", filename));
+}
+
+void cleanup() {
+	CloseAudioDevice(); 
+
+    CloseWindow();        // Close window and OpenGL context
 }
 
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-	
-	const int scarfySpeed = 5;
+    
 
     InitWindow(screenWidth, screenHeight, "RayLib - 2D Character Animation");
+	
+	InitAudioDevice();
+	
+	atexit(cleanup);
 
 	const char *filename = "scarfy.png";
 	Texture2D scarfy = LoadTexture(filename);
 	if(!isTextureValid(&scarfy)) {
-		
-		while (!WindowShouldClose()) {
-			BeginDrawing();
-				ClearBackground(RAYWHITE);
-				DrawText(TextFormat("ERROR: Couldn't load %s.", filename), 20, 20, 20, BLACK);
-			EndDrawing();
-		}
-		return 10;
+		showCantLoadFileErrorAndExit(filename);
 	}
-	
 	unsigned numFrames = 6;
 	int frameWidth = scarfy.width / numFrames;
 	Rectangle frameRec = { 0.0f, 0.0f, (float)frameWidth, (float)scarfy.height };
-	Vector2 scarfyPosition = {screenWidth / 2.0f, screenHeight / 2.0f};
+	Vector2 scarfyPosition = {screenWidth / 2.0f, groundYPos - scarfy.height};
     Vector2 scarfyVelocity = {0.0f,0.0f};
+	
+	filename = "Single-footstep-in-grass-A-www.fesliyanstudios.com.mp3";
+	Sound footstepSound = LoadSound(filename);
+	if(!footstepSound.frameCount) {
+		showCantLoadFileErrorAndExit(filename);
+	}
+	filename = "Single-footstep-in-grass-B-www.fesliyanstudios.com.mp3";
+	Sound landingSound = LoadSound(filename);
+	if(!landingSound.frameCount) {
+		showCantLoadFileErrorAndExit(filename);
+	}
 	
 	unsigned frameDelay = 5;
 	unsigned frameDelayCounter = 0;
@@ -75,30 +135,60 @@ int main(void)
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 		// Update
-		if (IsKeyDown(KEY_RIGHT) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) {
-			scarfyVelocity.x = scarfySpeed;
-			if(frameRec.width < 0) {
-				frameRec.width = -frameRec.width;
+		if (isScarfyOnGround(&scarfy, &scarfyPosition)) {
+			if (IsKeyDown(KEY_UP) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_UP)) {
+				scarfyVelocity.y = - 2 * scarfySpeed;
 			}
-        } else if (IsKeyDown(KEY_LEFT) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_LEFT)) {
-			scarfyVelocity.x = -scarfySpeed;
-			if(frameRec.width > 0) {
-				frameRec.width = -frameRec.width;
+			if (IsKeyDown(KEY_RIGHT) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) {
+				scarfyVelocity.x = scarfySpeed;
+				if(frameRec.width < 0) {
+					frameRec.width = -frameRec.width;
+				}
+			} else if (IsKeyDown(KEY_LEFT) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_LEFT)) {
+				scarfyVelocity.x = -scarfySpeed;
+				if(frameRec.width > 0) {
+					frameRec.width = -frameRec.width;
+				}
+			} else {
+				scarfyVelocity.x = 0;
 			}
-		} else {
-			scarfyVelocity.x = 0;
 		}
+		
 		bool scarfyMoving = scarfyVelocity.x != 0.0f || scarfyVelocity.y != 0.0f;
         //----------------------------------------------------------------------------------
+		
+		bool wasScarfyOnGround = isScarfyOnGround(&scarfy, &scarfyPosition);
 		scarfyPosition = Vector2Add(scarfyPosition, scarfyVelocity);
+		bool scarfyIsOnGround = isScarfyOnGround(&scarfy, &scarfyPosition);
+		if(scarfyIsOnGround) {
+			scarfyVelocity.y = 0;
+			scarfyPosition.y = groundYPos - scarfy.height;
+			if(!wasScarfyOnGround) {
+				PlaySound(landingSound);
+			}
+		} else {
+			scarfyVelocity.y += gravity;
+		}
 		
 		++frameDelayCounter;
 		if(frameDelayCounter > frameDelay) {
 			frameDelayCounter = 0;
 			
 			if(scarfyMoving) {
-				++frameIndex;
-				frameIndex %= numFrames;
+				if(scarfyIsOnGround) {
+					++frameIndex;
+					frameIndex %= numFrames;
+					
+					if(isFootstepFrame(frameIndex)) {
+						PlaySound(footstepSound);
+					}
+				} else {
+					if(scarfyVelocity.y < 0) {
+						frameIndex = jumpUpFrame;
+					} else {
+						frameIndex = jumpDownFrame;
+					}
+				}
 				frameRec.x = (float) frameWidth * frameIndex;
 			}
 		}
@@ -120,10 +210,9 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
 
-    // TODO: Unload all loaded resources at this point
-
-    CloseWindow();        // Close window and OpenGL context
+	cleanup();
+	atexit(NULL);
     //--------------------------------------------------------------------------------------
 
-    return 0;
+    return EXIT_SUCCESS;
 }
